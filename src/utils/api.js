9979e735923f89e3748e97fccfdbfebb3f1a7114
erin90523 <erin90523@naver.com>
2025-01-02 13:08:31 +0900

    수정4

--- src/utils/api.js
+++ src/utils/api.js
@@ -14,5 +14,6 @@ export async function sendMessage(messages) {
         throw new Error(`Failed to send message: ${errorText}`);
       }
     
-      return response.text();
+    const data = await response.json();
+    return data.reply;
 }
